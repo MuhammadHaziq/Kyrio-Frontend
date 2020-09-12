a1e660b16633c36d0e1c9586ac85f1b0331a6de0
unknown <Administrator@pos.kyrio.cloud>
2020-09-12 09:28:53 -0500

    changes

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -30,7 +30,7 @@ export const login = (data) => {
               typeof error.response != "undefined"
                 ? error.response.data.message
                 : ERROR_MESSAGE,
-            object: error.response.data,
+            object: error,
             error: true,
           };
           dispatch({ type: MESSAGE, data: msg });
@@ -75,7 +75,7 @@ export const signup = (data) => {
               typeof error.response != "undefined"
                 ? error.response.data.message
                 : ERROR_MESSAGE,
-            object: error.response.data,
+            object: error,
             error: true,
           };
           dispatch({ type: MESSAGE, data: msg });

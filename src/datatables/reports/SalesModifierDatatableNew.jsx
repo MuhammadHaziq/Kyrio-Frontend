e85376df6deece257e8adc37cf41f7964da8b9e7
TahirAmjad <tahiramjad79@gmail.com>
2021-12-28 21:10:45 +0000

    changes done for modifiers

--- src/datatables/reports/SalesModifierDatatableNew.jsx
+++ src/datatables/reports/SalesModifierDatatableNew.jsx
@@ -1,22 +1,33 @@
 import React, { useState, useEffect } from "react";
 import {
-  CDataTable
+  CDataTable,
+  // CCollapse
 } from "@coreui/react";
 import ModifierIcon from '../../assets/icons/modifierIcon.svg'
+import { useSelector } from "react-redux";
+import Amount from "../../components/utils/Amount";
 
 const SalesModifierDatatableNew = (props) => {
-  
+
+  const darkMode = useSelector((state) => state.settings.darkMode);
+  // const [collapsed, setCollapsed] = React.useState(Array(props.sale_modifier_summary.length).fill(true));
   const [fields, setFields] = useState([]);
   const [fieldLength, setFieldLength] = useState(0);
 
   useEffect(() => {
-    if(props.columns.length > 0){
+    if (props.columns.length > 0) {
       let result = props.columns.filter(itm => itm.isShow);
-      setFieldLength(100/result.length)
+      setFieldLength(100 / result.length)
       setFields(result)
     }
-  },[props.columns])
+  }, [props.columns])
   
+
+  // const onRowClick = (e,i) => {
+  //   let shadow = collapsed;
+  //   shadow[i] = !shadow[i];
+  //   setCollapsed(shadow);
+  // }
   return (
     <CDataTable
       itemsPerPageSelect
@@ -28,75 +39,78 @@ const SalesModifierDatatableNew = (props) => {
       hover
       outlined
       pagination
-      scopedSlots = {{
-        'Modifier': (item)=>(
-          <td style={{ borderRight: "1px solid #d8dbe0", width: fieldLength+"%" }}>
+      // onRowClick={onRowClick}
+      scopedSlots={{
+        'Modifier': (item) => (
+          <td style={{ borderRight: "1px solid #d8dbe0", width: fieldLength + "%" }}>
             <strong>
-            <img
-          src={ModifierIcon}
-          className="c-sidebar-brand-full"
-          alt="kyrio POS"
-          style={{ width: "40px", height: "40px" }}
-          />&nbsp;&nbsp;
+              <img
+                src={ModifierIcon}
+                className="c-sidebar-brand-full"
+                alt="kyrio POS"
+                style={{ width: "40px", height: "40px" }}
+              />&nbsp;&nbsp;
               {item.Modifier}</strong>
           </td>
         ),
-        'quantitySold': (item)=>(
+        'quantitySold': (item) => (
           <td>
             <strong>{item.quantitySold}</strong>
           </td>
         ),
-        'grossSales': (item)=>(
+        'grossSales': (item) => (
           <td>
-            <strong>{item.grossSales}</strong>
+            <strong>{<Amount value={item.grossSales} />}</strong>
           </td>
         ),
-        'refundQuantitySold': (item)=>(
+        'refundQuantitySold': (item) => (
           <td>
             <strong>{item.refundQuantitySold}</strong>
           </td>
         ),
-        'refundGrossSales': (item)=>(
+        'refundGrossSales': (item) => (
           <td>
-            <strong>{item.refundGrossSales}</strong>
+            <strong>{<Amount value={item.refundGrossSales} />}</strong>
           </td>
         ),
-        'net_sales': (item)=>(
+        'net_sales': (item) => (
           <td>
-            <strong>{parseFloat(item.grossSales - item.refundGrossSales).toFixed(2)}</strong>
+            <strong>{<Amount value={(item.grossSales - item.refundGrossSales)} />}</strong>
           </td>
         ),
         'details':
-            (item, index)=>{
-              return (
-                <table style={{width: "100%"}}>
+          (item, index) => {
+            return (
+              // <CCollapse show={collapsed[index]}>
+                <table style={{ width: "100%", backgroundColor: darkMode ? "rgba(255, 255, 255, 0.1)" : "#F5F5F5" }}>
                   <tbody>
                     {item.options.map((op) => {
-                        return (<tr>
-                          {fields.find(item => item.key == "Modifier")?.isShow ? 
-                          <td style={{ width: fieldLength+"%", borderRight: "1px solid #d8dbe0", paddingLeft: "6%" }}><span>{op.Option}</span></td>
-                          : "" }
-                          {fields.find(item => item.key == "quantitySold")?.isShow ? 
-                          <td style={{ width: fieldLength+"%" }}>{op.quantitySold}</td>
-                          : "" }
-                          {fields.find(item => item.key == "grossSales")?.isShow ? 
-                          <td style={{ width: "16%" }}>{op.grossSales}</td>
-                          : "" }
-                          {fields.find(item => item.key == "refundQuantitySold")?.isShow ? 
-                          <td style={{ width: fieldLength+"%" }}>{op.refundQuantitySold}</td>
-                          : "" }
-                          {fields.find(item => item.key == "refundGrossSales")?.isShow ? 
-                          <td style={{ width: fieldLength+"%" }}>{op.refundGrossSales}</td>
-                          : "" }
-                          {fields.find(item => item.key == "net_sales")?.isShow ? 
-                          <td style={{ width: fieldLength+"%" }}>{parseFloat(op.grossSales - op.refundGrossSales).toFixed(2)}</td>
-                          : "" }
-                          </tr>
-                        )
+                      return (<tr>
+                        {fields.find(item => item.key == "Modifier")?.isShow ?
+                          <td style={{ width: fieldLength + "%", paddingLeft: "6%" }}><span>{op.Option}</span></td>
+                          : ""}
+                        {fields.find(item => item.key == "quantitySold")?.isShow ?
+                          <td style={{ width: fieldLength + "%" }}>{op.quantitySold}</td>
+                          : ""}
+                        {fields.find(item => item.key == "grossSales")?.isShow ?
+                          <td style={{ width: "16%" }}>{<Amount value={op.grossSales} />}</td>
+                          : ""}
+                        {fields.find(item => item.key == "refundQuantitySold")?.isShow ?
+                          <td style={{ width: fieldLength + "%" }}>{op.refundQuantitySold}</td>
+                          : ""}
+                        {fields.find(item => item.key == "refundGrossSales")?.isShow ?
+                          <td style={{ width: fieldLength + "%" }}>{<Amount value={op.refundGrossSales} />}</td>
+                          : ""}
+                        {fields.find(item => item.key == "net_sales")?.isShow ?
+                          <td style={{ width: fieldLength + "%" }}>{<Amount value={(op.grossSales - op.refundGrossSales)} />}</td>
+                          : ""}
+                      </tr>
+                      )
                     })}
-                    
+
                   </tbody>
-                  </table>
+                </table>
+              // </CCollapse>
             )
           }
       }}
